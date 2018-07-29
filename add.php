3ac41b13dae15b34565c6f79888038c286bd1272
sidiqi <cssitemail@gmail.com>
2018-07-29 21:10:47 +0300

    add form

--- add.php
+++ add.php
@@ -1,38 +1,27 @@
 <?php
 	 
-	 if(isset($_POST['add'])){
+	 if(isset($_POST['table_name'])){
 	$mysqli = new mysqli ("project.local", "root", "root", "tvbox");
 	$mysqli->query ("SET NAMES 'utf-8'");
 
 	$name = $_POST['name'];
 	$model = $_POST['model'];
 	$placement = $_POST['placement'];
+	$time = $_POST['time'];
+	$date = $_POST['date'];
+	$status = $_POST['status'];
+	$video = $_POST['video'];
 	// $status = 'Ok';
 	// $video = 'URL';
 
-	$success = $mysqli->query ("INSERT INTO `console` (`name`, `model`, `placement`, `status`, `video`) VALUES ('$name', '$model', '$placement', 'status', '1')");
+	$success = $mysqli->query ("INSERT INTO `console` (`name`, `model`, `placement`, `status`, `video`, `time`, `date`) VALUES ('$name', '$model', '$placement', '$status', '1', '$time', '$date')");
 
-	echo 'Приставка успешно добавлена!';
+	$ok = 'Приставка успешно добавлена!';
+	
+ echo '<div class="alert alert-success" role="alert">' . $ok . '</div>';
+
 
 
 }
 
 ?>
-
-
-	<form method="POST" action="add.php">
-			
-			Марка<br><input type="text" name="name"/><br>
-			Модель<br><input type="text" name="model"/><br><br>
-			
-			<select name="placement">
-  				<option value="1">Омега</option>
- 				<option value="2">Торговый квартал</option>
- 				<option value="3">Сити Молл</option>
- 				<option value="4">Sunrise</option>
- 				<option value="5">Палитра</option>
-			</select>
-
-			<input type="submit" name="add" value="добавить" />
-
-	</form>

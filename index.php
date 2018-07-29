d1f53fcd737f325b649e452b2779fa3a6cb374e4
sidiqi <cssitemail@gmail.com>
2018-07-29 21:11:46 +0300

    added search bar

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
   
 	</script> 
 
-	<!-- <script type="text/javascript">
+<!-- 	 <script type="text/javascript">
 
 	$(document).on('click', '#button2', function() {
 
@@ -47,22 +47,53 @@
 			}); 
 		});
   
-	</script> 
- -->
+	</script>  -->
+ 
+	<style type="text/css">
+		a {
+			text-decoration: none; 
+			color: white;
+		}
+
+		a:hover {
+			text-decoration: none; 
+			color: white;
+		}
+		
+		.btn1 {
+			width: 100px!important;
+			border-radius: 0px;
+			margin: 2px!important;
+		}
+
+		.btn1 {
+			
+			border-radius: 0px;
+			
+		}
+
+		.input-group-text {
+			width: 90px!important;
+		}
+
+
+
+	</style>
+
 	</head>
 
 	<body>
 
 	 <nav style="background-color: black" class="navbar navbar-expand-lg navbar-dark">
 			<div class="container">
-				<a class="navbar-brand" href="#">Tv Box: Control center</a>
+				<a class="navbar-brand" href="index.php">Tv Box: Control center</a>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>
 				<div class="collapse navbar-collapse" id="navbarResponsive">
 					<ul class="navbar-nav ml-auto">
 						<li class="nav-item active">
-							<a class="nav-link" href="#">LOG OUT</a>
+							<a class="nav-link" href="">LOG OUT</a>
 						</li>
 					</ul>
 				</div>
@@ -77,53 +108,49 @@
 		<div class="row">
 			
 			<div class="col-md-4">
-				<h5>Проверить приставку</h5><hr>
+				<h5>Найти приставку</h5><hr>
 				<div class="input-group mb-3">
-					<div class="input-group-prepend">
-						<label class="input-group-text" for="inputGroupSelect01">ТВ Приставка</label>
-					</div>
-				
-					<select name="select" class="custom-select" id="inputGroupSelect01">
-						<option selected>Выберите...</option>
-						<option id="1" value="1">Sony B130</option>
-						<option value="2">XIAOMI MI TV BOX 3</option>
-						<option value="3">Rombica Cinema 4k v02</option>
-						<option value="4">iconBIT PC-0035W</option>
-						<option value="5">Supra	SDT-67</option>	
-					</select>
-				</div>
+  <input type="text" class="form-control" placeholder="Введите название" aria-label="Recipient's username" aria-describedby="button-addon2">
+  <div class="input-group-append">
+    <button class="btn btn-primary" type="button" id="button-addon2">Поиск</button>
+  </div>
+</div>
 			
 
 			<div name="table_name" id="table_name"> </div>
+				
+			<button id="ShowAll" type="button" class="btn btn-outline-primary">Показать все приставки</button>
+
 			
-			<button id="button2" type="button" class="btn btn-primary">Выбрать</button> ИЛИ
-
-			<button id="ShowAll" type="button" class="btn btn-outline-primary">Проверить все приставки</button>
-
-			<Br>
-			<Br>
-			<Br>
-			<Br>
-			<Br>
+			<br>
+			<br>
+			<br>
 			<h5>Добавить приставку</h5><hr>
 
-<!-- 			<div class="input-group input-group-default mb-3">
+
+	<form method="POST" action="index.php">
+ 			<div class="input-group input-group-default mb-3">
 			  <div class="input-group-prepend">
 			    <span class="input-group-text" id="inputGroup-sizing-default">Марка</span>
 			  </div>
-			  <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+			  <input name="name" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" required>
 			</div>
+
 			<div class="input-group input-group-default mb-3">
 			  <div class="input-group-prepend">
 			    <span class="input-group-text" id="inputGroup-sizing-default">Модель</span>
 			  </div>
-			  <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+			  <input name="model" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
 			</div>
+
+			<input type="hidden" name="time" value=" <?php echo date('H:i');?> " />
+			<input type="hidden" name="date" value=" <?php echo date('Y-m-d');?> " />
+
 			<div class="input-group mb-3">
 			  <div class="input-group-prepend">
-			    <label class="input-group-text" for="inputGroupSelect01">Расположение</label>
+			    <label class="input-group-text" for="inputGroupSelect01">Место</label>
 			  </div>
-			  <select class="custom-select" id="inputGroupSelect01">
+			  <select name="placement" class="custom-select" id="inputGroupSelect01">
 			    <option selected>Выберите</option>
 			    <option value="1">Омега</option>
 			    <option value="2">Торговый квартал</option>
@@ -132,23 +159,30 @@
 			    <option value="5">Палитра</option>
 			  </select>
 			</div>
+
+			<div class="input-group mb-3">
+			  <div class="input-group-prepend">
+			    <label class="input-group-text" for="inputGroupSelect01">Статус</label>
+			  </div>
+			  <select name="status" class="custom-select" id="inputGroupSelect01">
+			    <option selected>Выберите</option>
+			    <option value="Online">Online</option>
+			    <option value="Offline">Offline</option>
+			  </select>
+			</div>
+
 			<div class="input-group input-group-default mb-3">
 			  <div class="input-group-prepend">
-			    <span class="input-group-text" id="inputGroup-sizing-default">Видео (URL)</span>
+			    <span class="input-group-text" id="inputGroup-sizing-default">Видео</span>
 			  </div>
 			  <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
 			</div>
-			<button id="Add" type="button" class="btn btn-primary">Добавить</button>
- -->
+			<input value="Добавить" type="submit" name="add" id="Add" type="button" class="btn btn-primary"/>
+ 
+	</form>
 
 
-			<form method="POST" action="add.php">
-			
-			Марка<br><input type="text" name="name"/><br>
-			Модель<br><input type="text" name="model"/><br><br>
-			<input type="submit" name="add" value="добавить" />
 
-			</form>
 
 
 			</div>
@@ -165,8 +199,52 @@
 
 
 			<div id="table"></div>
-			
+			<?php
+require_once 'connection.php';
+if(isset($_GET['id']))
+{   
+    $link = mysqli_connect($host, $user, $password, $database) 
+            or die("Ошибка " . mysqli_error($link)); 
+    $id = mysqli_real_escape_string($link, $_GET['id']);
+     
+    $query ="DELETE FROM console WHERE ConsoleID = '$id'";
+    echo '<div class="alert alert-success" role="alert">
+  Приставка успешно удалена!
+</div>';
+ 
+    $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link)); 
+    mysqli_close($link);
+}
+?>
 				<!-- Сюда приходят данные от ajax -->
+
+<?php
+	 
+	 if(isset($_POST['add'])){
+	$mysqli = new mysqli ("project.local", "root", "root", "tvbox");
+	$mysqli->query ("SET NAMES 'utf-8'");
+
+	$name = $_POST['name'];
+	$model = $_POST['model'];
+	$placement = $_POST['placement'];
+	$time = $_POST['time'];
+	$date = $_POST['date'];
+	$status = $_POST['status'];
+	$video = $_POST['video'];
+	// $status = 'Ok';
+	// $video = 'URL';
+
+	$success = $mysqli->query ("INSERT INTO `console` (`name`, `model`, `placement`, `status`, `video`, `time`, `date`) VALUES ('$name', '$model', '$placement', '$status', '1', '$time', '$date')");
+
+	$ok = 'Приставка успешно добавлена!';
+	
+ echo '<div class="alert alert-success" role="alert">' . $ok . '</div>';
+
+
+
+}
+
+?>
 
 			</div>
 	

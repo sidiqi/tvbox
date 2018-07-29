a3b5c4ee636156fd90af3eb9ba35650e72ae720f
sidiqi <cssitemail@gmail.com>
2018-07-29 21:13:00 +0300

    create/update form

--- functions.php
+++ functions.php
@@ -1,11 +1,16 @@
+
+
+
 <?php
 	
+
 if (isset ($_POST['table_name'])) {
+$select = $_POST['select'];
 require_once 'connection.php'; 
 $link = mysqli_connect($host, $user, $password, $database) ;
-$query ="SELECT * FROM console INNER JOIN placement ON console.placement=placement.PlacementID INNER JOIN video ON console.video=video.VideoID";
+$query ="SELECT * FROM console INNER JOIN placement ON console.placement=placement.PlacementID INNER JOIN video ON console.video=video.VideoID ORDER BY ConsoleID DESC";
 
-
+//
 
 $result = mysqli_query($link, $query); 
 
@@ -19,31 +24,41 @@ $result = mysqli_query($link, $query);
 
 		// json_encode($response);
 
- 		$html .= '<ul class="list-unstyled">';
+ 		
+		foreach ($response as $key) { ?>
+	<div class="row">
+		<div class="col-md-9">
+	<ul class="list-unstyled">
+		 <li><b> Приставка: </b><?php echo $key['name'] ?> <?php echo $key['model'] ?></li>
+		 <li><b> id приставки: </b> <?php echo $key['ConsoleID'] ?> </li> 
+		 <li><b>Статус: </b><font color="green"><?php echo $key['status'] ?></font></li> 
+		 <li><b>Расположение: </b><?php echo $key['placement_name'] ?></li>
+		 <li><b>Тип: </b><?php echo $key['type'] ?></li>
+		 <li><b>Видео: </b><?php echo $key['VideoName'] ?> <a href="#">link</a></li>
+		 <li><b>Дата добавления приставки: </b><?php echo $key['date'] ?> <?php echo $key['time'] ?></li></ul>
+		 
+	</ul>
+	</div>
+	<div class="col-md-3">
+		<button type="button" class="btn btn1 btn-sm btn-danger"><a href="index.php?id=<?php echo $key['ConsoleID']?>">Удалить</a></button>
+		<button type="button" class="btn btn1 btn-sm btn-success"><a href="index.php?id=<?php echo $key['ConsoleID']?>">Изменить</a></button>
+	</div>
 
-		foreach ($response as $key) {
-
-		 $html .= 
-		 '<li><b> Приставка: </b>' . $key['name'] . ' ' . $key['model'] . '</li>' . 
-		 '<li><b>Статус: </b>' . '<font color="green">' . $key['status']. '</font></li>' .  
-		 '<li><b>Расположение: </b>' . $key['placement_name'] . '</li>' .
-		 '<li><b>Тип: </b>' . $key['type'] . '</li>' .
-		 '<li><b>Видео: </b>' . $key['VideoName'] . ' <a href="#">link</a>' .'</li><hr>' 
-
-		 ;
-		
+	</div>
+		<hr>
+		<?php
 		}
 
-		$html .= '</ul>';
-
-		
-		echo $html;
     
   }   
   	else {
   		echo '<h2>Приставка не найдена!</h2>';
   	}	
 }
+
+
+	$id = $_GET['ConsoleID'];
+	//mysql_query("DELETE FROM console WHERE ConsoleID='$id'");
 
 
 ?>
